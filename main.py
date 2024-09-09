15c587dfbab794b4ea898cad78900a9de786c214
bestdevmgp <pmg3858@icloud.com>
2024-09-10 00:18:47 +0900

    Intial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# import calculator
+#
+# a, b = map(int, input().split())
+# add_result = calculator.add(a, b)
+# sub_result = calculator.sub(a, b)
+#
+# print(add_result)
+# print(sub_result)
+
+import hello
+
+print(hello.hi('MaAaram'))
+print(hello.__name__)
+print(__name__)
